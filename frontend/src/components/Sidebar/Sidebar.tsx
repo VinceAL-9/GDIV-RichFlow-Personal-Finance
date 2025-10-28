3c93f874b9bec8d5bb6aafa9e645ad9f163766d5
johquendo <johancarlos.oquendo-24@cpu.edu.ph>
2025-10-28 22:28:40 +0800

    style: made that the sidebar expands when hovered

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -2,23 +2,60 @@ import React from 'react';
 import './Sidebar.css';
 
 const Sidebar: React.FC = () => {
+  const [expanded, setExpanded] = React.useState(false);
+
   return (
-    <aside className="sidebar">
+    <aside className={`sidebar ${ expanded ? 'expanded' : ''}` } onMouseEnter={() => setExpanded(true)} onMouseLeave={() => setExpanded(false)}>
       <div className="sidebar-section">
-        <button className="sidebar-button small"></button>
+
+        <button className="selection small">
+          <div className="sidebar-button small"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
       </div>
       <div className="sidebar-section">
-        <button className="sidebar-button large"></button>
-        <button className="sidebar-button large"></button>
-        <button className="sidebar-button large"></button>
+        <button className="selection large"> 
+          <div className="sidebar-button large"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
+        <button className="selection large"> 
+          <div className="sidebar-button large"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
+        <button className="selection large"> 
+          <div className="sidebar-button large"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
       </div>
       <div className="sidebar-section">
-        <button className="sidebar-button small"></button>
+
+        <button className="selection small">
+          <div className="sidebar-button small"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
       </div>
+
       <div className="sidebar-section">
-        <button className="sidebar-button large"></button>
-        <button className="sidebar-button large"></button>
-        <button className="sidebar-button large"></button>
+
+        <button className="selection large"> 
+          <div className="sidebar-button large"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
+        <button className="selection large"> 
+          <div className="sidebar-button large"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
+        <button className="selection large"> 
+          <div className="sidebar-button large"></div>
+          <span className="sidebar-text"> Placeholder </span>
+        </button>
+
       </div>
     </aside>
   );
