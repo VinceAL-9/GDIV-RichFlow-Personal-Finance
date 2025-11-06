1884ed77d7315729a90ecd38cbff3df65c1753cd
Vince <vinceamiel.latabe-24@cpu.edu.ph>
2025-11-06 14:10:05 +0800

    feat: update header to display user name and email in a structured layout

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -10,7 +10,10 @@ const Header: React.FC = () => {
       <div className="header-left">
         <div className="logo">
           <div className="logo-circle max-h-fit"><img src="../../../assets/richflow.png" alt="RichFlow Logo" className="logo-icon" /></div>
-          <span className="logo-text">{user?.name || 'RichMan'}</span>
+          <div className="flex flex-col">
+            <span className="logo-text">{user!.name}</span>
+            <span className="text-white text-sm opacity-80">{user!.email}</span>
+          </div>
         </div>
       </div>
       <div className="header-center">
